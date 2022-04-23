1921058b86ed327d28b4c6e7b2fe09d9ab419934
Potrijo <backinmarc@gmail.com>
2022-04-23 23:01:44 +0200

    Hibernate con annotaciones TODO HECHO

--- HibernateAnotaciones03/src/main/java/com/marc/HibernateAnotaciones03/App.java
+++ HibernateAnotaciones03/src/main/java/com/marc/HibernateAnotaciones03/App.java
@@ -5,7 +5,25 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 public class App {
+	
+	public static void nativeQuery() {
+	SessionFactory sessionFactory= new Configuration().configure().buildSessionFactory();
+	Session session = sessionFactory.openSession();
+	List<Libros> libros = session.createNativeQuery(
+		"SELECT * "
+		+"FROM libros "
+		+"WHERE titulo like :name")
+		.addEntity(Libros.class)
+		.setParameter("name", "M%")
+		.list();
+	for (Libros libro: libros) {
+		System.out.println(libro.getTitulo());
+	}
+	session.close();
+	}
+	
 	public static void main(String[] args) {
+		System.out.println("Parte 2.2");
 		@SuppressWarnings("unused")
 		 org.jboss.logging.Logger logger = org.jboss.logging.Logger.getLogger("org.hibernate");
 		 java.util.logging.Logger.getLogger("org.hibernate").setLevel(java.util.logging.Level.SEVERE);
@@ -26,5 +44,8 @@ public class App {
 			+ resultado.getAnyo());
 			 }
 		session.close();
+		
+		System.out.println("Parte 3");
+		nativeQuery();
 	 }
 }
